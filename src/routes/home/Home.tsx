ea580e5ae236ab6dd370700166ba2f857f8d8fa1
Willis Shek <willis.shek@gmail.com>
2022-03-03 19:20:24 +1100

    Construct Home page

---
+++ src/routes/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+// components
+import Navigator from "components/Navigator/Navigator";
+// styles
+import "./Home.scss";
+
+export default function Home() {
+	return (
+		<div className="site-home">
+			<Navigator />
+		</div>
+	);
+}
