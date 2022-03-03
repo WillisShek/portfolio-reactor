1dc269d6b14e28007169d569dafb89949419dd55
Willis Shek <willis.shek@gmail.com>
2022-03-03 19:20:09 +1100

    Add models for npmPackages

---
+++ src/models/npmPackages.ts
@@ -0,0 +1,29 @@
+// contain only the necessary structures of a package object
+export type NpmPackage = {
+	name: string;
+	// some package has no author, e.g. react
+	author?: string;
+	date: string;
+	link: string;
+};
+
+export type NpmPackages = NpmPackage[];
+
+export type NpmPackageFromAPI = NpmPackage & {
+	package: {
+		name: string;
+		// some package has no author, e.g. react
+		author?: {
+			name: string;
+			// we don't care about the rest
+			[key: string]: unknown;
+		};
+		date: string;
+		links: {
+			npm: string;
+			[key: string]: unknown;
+		};
+		[key: string]: unknown;
+	};
+	[key: string]: unknown;
+};
